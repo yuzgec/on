478a704fdbf3fa771d47b63414feb89efebc8162
yuzgec <olcayy@gmail.com>
2024-03-18 18:20:46 +0300

    fix

--- resources/views/frontend/page/index.blade.php
+++ resources/views/frontend/page/index.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="container-md">
        <div class="t-center mt-25">
-          <h5 class="fs-11 ls-4 semibold white uppercase"> ONDANCE STUDIO - #burasiizmir </h5>
+          <h5 class="fs-11 ls-4 semibold white"> ONDANCE STUDIO - #burasiizmir </h5>
           <h1 class="mt-15 font-secondary lh-md"> {{ $Detay->title }}</h1>
           <div class="mt-30 uppercase fs-12 bold bg-soft-dark3 radius-lg py-10 px-40 d-inline-flex width-auto lh-normal align-items-center">
              <a href="index.html"><i class="ti-home"></i></a>Anasayfa<i class="ti-angle-right fs-7 mx-15"></i> 
@@ -18,35 +18,33 @@
        </div>
     </div>
  </section>
-<div id="elementDescription" class="container py-100 py-100-sm">
 
+ <div id="elementDescription" class="container py-100 py-100-sm">
     <span class="fs-20 fs-16-sm gray7 ls-0 lh-35 light">
         {!! $Detay->desc !!}
     </span>
-
 </div>
 
 <section id="service-boxes-02" class="pt-30 pb-60 bg-gray2 bt-1 b-gray1">
     <div class="container">
-
         <div class="col t-center">
             <h2 class="lh-45 mt-10 uppercase">Eğitimlerimiz</h2>
         </div>
-
         <div class="row t-left t-center-sm align-items-center">
-
-           
             @foreach ($Service->where('category', 1) as $item)
             <div class="col-lg-4 col-12 mt-30 perspective-lg relative zi-hover">
                 <div class="bg-white bs-lg-hover dark2 slow c-default py-40 px-40">
-                    <h5 class="fs-18 medium mt-25">
-                        <a href="{{ route('service', $item->slug)}}" title="{{ $item->title}}">
-                            {{ $item->title}}
-                        </a>
-                    </h5>
-                    <p class="fs-16 lh-25 mt-15 light">
-                        In sit amet posuere felis. Suspendisse finibus viverra justo eget ullamcorper.
-                    </p>
+                    <a href="{{ route('service', $item->slug)}}" class="post-details" alt="{{ $item->title}} - İzmir On Dance Studio">
+                        <div class="d-flex justify-content-between">    
+                            <h4 class="post-title">
+                                {{ $item->title}}
+                            </h4>
+                        
+                            <span class="badge badge-success">
+                                {{ $item->short }}
+                            </p>
+                        </div>
+                    </a>
                 </div>
             </div>
 
