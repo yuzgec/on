444a83610ce7b0d747b85070292bb157ad966a2d
yuzgec <olcayy@gmail.com>
2024-04-16 14:41:00 +0300

    Fix

--- resources/views/livewire/sms-counter.blade.php
+++ resources/views/livewire/sms-counter.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <button class="btn btn-sm btn-primary" wire:click="smsCounter">SMS Kontur Sorgula</button>
+    <button class="btn btn-sm btn-primary" wire:click="counter">SMS Kontur Sorgula</button>
 </div>
