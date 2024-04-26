99d7ffbeb251d0f21290d1408147b3022af5ccf7
yuzgec <olcayy@gmail.com>
2024-04-26 22:40:10 +0300

    Fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,17 +11,21 @@ class OrderController extends Controller
 {
     public function index(){
         $All = ShopCart::withCount('getOrder')->get();
-        //dd($All);
         return view('backend.order.index', compact('All'));
     }
 
     public function show($id){
+
+        $Invoice = null;
+        $Download = null;
         
         $Detail = ShopCart::with('getOrder')->where('cart_id',$id)->withCount('getOrder')->first();
-        
-        $gib = (new Gib)->setCredentials(config('settings.earsiv_portal_user'), config('settings.earsiv_portal_pass'))->login();
-        $Invoice = $gib->getHtml($Detail->invoice_id);
-        $Download = $gib->getDownloadURL($Detail->invoice_id);
+        if($Detail->invoice_id){
+            $gib = (new Gib)->setCredentials(config('settings.earsiv_portal_user'), config('settings.earsiv_portal_pass'))->login();
+            $Invoice = $gib->getHtml($Detail->invoice_id);
+            $Download = $gib->getDownloadURL($Detail->invoice_id);
+        }
+     
 
         return view('backend.order.show', compact('Detail', 'Invoice', 'Download'));
     }
